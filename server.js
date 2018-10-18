ab15200c661d1d516d6ef490b25c618e3c2bda08
Sven Grunwald <sveng@Svens-MBP.fritz.box>
2018-10-18 11:44:35 +0200

    Initial commit for express-app

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to write to file!');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'Home Page',
+        welcomeText: 'Welcome to my page!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'This is a test'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error. Something went wrong!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
+});
